1fa5a9dadee0069e230c5fcab9808bff46e1a870
dollypizzle <dolapo.mabawonku@gmail.com>
2019-12-11 13:13:39 +0100

    Adjustment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 
-docker build -t dollypizzle/ecommerce-client:latest -t dollypizzle/ecommerce-client:$SHA -f ./client/Dockerfile ./client
-docker build -t dollypizzle/ecommerce-server:latest -t dollypizzle/ecommerce-server:$SHA -f ./server/Dockerfile ./server
+docker build -t dollypizzle/eco-client:latest -t dollypizzle/eco-client:$SHA -f ./client/Dockerfile ./client
+docker build -t dollypizzle/eco-server:latest -t dollypizzle/eco-server:$SHA -f ./server/Dockerfile ./server
 
-docker push dollypizzle/ecommerce-client:latest
-docker push dollypizzle/ecommerce-server:latest
+docker push dollypizzle/eco-client:latest
+docker push dollypizzle/eco-server:latest
 
-docker push dollypizzle/ecommerce-client:$SHA
-docker push dollypizzle/ecommerce-server:$SHA
+docker push dollypizzle/eco-client:$SHA
+docker push dollypizzle/eco-server:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=dollypizzle/ecommerce-server:$SHA
-kubectl set image deployments/client-deployment client=dollypizzle/ecommerce-client:$SHA
+kubectl set image deployments/server-deployment server=dollypizzle/eco-server:$SHA
+kubectl set image deployments/client-deployment client=dollypizzle/eco-client:$SHA
